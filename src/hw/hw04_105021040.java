d00896120db6caca29839b2845a83fcae5e30657
Shengjie0266 <105021040@live.asia.edu.tw>
2016-10-21 12:10:21 +0800

    hw4
    hw

--- src/hw/hw04_105021040.java
+++ src/hw/hw04_105021040.java
@@ -13,29 +13,63 @@ public class hw04_105021040 {
 			switch(val){
 			case 1:
 				System.out.println("請輸入正整數n");
-				int h = scn.nextInt();
-				int n=(h+1)/2;
+				int h1 = scn.nextInt();
+				int n=(h1+1)/2;
 				int flat=0;
-				for(int a=1;a<=h;a++){
+				for(int a=1;a<=h1;a++){
 					for(int b=1;b<=n;b++){
-						System.out.print("*");
+						System.out.print("@");
 					}
 					System.out.println("");
 //					System.out.print(n);
 					if(flat==0 && n>0){
 						n--;
 					}
-					if(flat==1&&n<(h+1)/2){
+					if(flat==1&&n<(h1+1)/2){
 						n++;
 					}
 					if(n==1){
 						flat=1;
 					}
 				}
-				break;
+			case 2:
+				int h2=scn.nextInt();
+				int m=1;
+				int flat1=0;
+				for(int a1=1;a1<=h2;a1++){
+					for(int b1=1;b1<=m;b1++){
+						System.out.print("%");
+					}
+					System.out.println("");
+					if(flat1==0 && m<(h2+1)/2){
+						m++;
+					}
+					if(flat1==1 && m>=1){
+						m--;
+					}
+					if(m==(h2+1)/2){
+						flat1=1;
+					}	
+				}
+			case 3:
+				int h3=scn.nextInt();
+				int o=1;
+				int flat2=0;
+				int p=(h3-1)/2;
+				for(int a2=1;a2<=h3;a2++){
+					for(int b2=1;b2<=o;b2++){
+					System.out.print(" ");
+					}
+					for(int b3=1;b3<=0;b3++){
+					System.out.println("#");
+					}
+					if(flat2==0 && o>0){
+						o--;
+					}
+					
+				}
 			}
 		}
-		System.out.print("掰掰");
 	}
 
 }
